331cc4a252514fb7791affb09ee8277d5b1ea7fd
Caio <caiiochaves82@gmail.com>
2024-07-15 19:39:38 -0300

    :goal_net: error-treatment: Evita mau funcionamento do banco de dados em possíveis erros.

--- Burgers/BurgerRouter.cs
+++ Burgers/BurgerRouter.cs
@@ -11,16 +11,18 @@ public static class BurgerRouter {
 
         // POSTs
          rotasBurgers.MapPost("burgers",
-         async (AddBurgerRequest request, AppDbContext context) => {
+        //  o CancellationToken é uma boa prática que garante o bom funcionamento do banco de dados em caso de
+        // fechamento indevido da aplicação enquanto ainda houver persistência do BD;
+         async (AddBurgerRequest request, AppDbContext context, CancellationToken ctoken) => {
             var jaExiste = await context.Burgers
-            .AnyAsync(Burger => Burger.Nome == request.Nome);
+            .AnyAsync(Burger => Burger.Nome == request.Nome, ctoken);
 
             if (jaExiste)
             return Results.Conflict("Estudante já existe.");
 
             var newBurger = new Burger(request.Nome);
-            await context.Burgers.AddAsync(newBurger);
-            await context.SaveChangesAsync();
+            await context.Burgers.AddAsync(newBurger, ctoken);
+            await context.SaveChangesAsync(ctoken);
 
             return Results.Ok(newBurger);
          });
@@ -28,42 +30,42 @@ public static class BurgerRouter {
         
 
         // GETs
-        rotasBurgers.MapGet("", async (AppDbContext appDbContext) => {
+        rotasBurgers.MapGet("", async (AppDbContext appDbContext, CancellationToken ctoken) => {
             var burgers = await appDbContext
             .Burgers
             .Where(burgers => burgers.Ativo == true)
             .Select(burger => new BurgerResponseDto(burger.Id, burger.Nome))
-            .ToListAsync();
+            .ToListAsync(ctoken);
             return burgers;
         });
 
         // PUTs
         rotasBurgers.MapPut("{id}",
-        async (UpdateBurgerRequest request, AppDbContext context, Guid id) => {
+        async (UpdateBurgerRequest request, AppDbContext context, Guid id, CancellationToken ctoken) => {
             var burger = await context.Burgers
-            .SingleOrDefaultAsync(burger => burger.Id == id);
+            .SingleOrDefaultAsync(burger => burger.Id == id, ctoken);
 
             if (burger == null) return Results.NotFound("Objeto não encontrado");
 
             burger.atualizarNome(request.nome);
 
-            await context.SaveChangesAsync();
+            await context.SaveChangesAsync(ctoken);
             return Results.Ok(new BurgerResponseDto(burger.Id, burger.Nome));
         });
 
         // DELETEs
         rotasBurgers.MapDelete("{id}",
-        async (Guid id, AppDbContext context) =>
+        async (Guid id, AppDbContext context, CancellationToken ctoken) =>
         {
             var burger = await context.Burgers
-            .SingleOrDefaultAsync(burger => burger.Id == id);
+            .SingleOrDefaultAsync(burger => burger.Id == id, ctoken);
 
             if (burger == null) return Results.NotFound("Objeto não encontrado");
             if (burger.Ativo == false) return Results.Conflict("Objeto já desativado");
 
             burger.desativar();
 
-            await context.SaveChangesAsync();
+            await context.SaveChangesAsync(ctoken);
             return Results.Ok();
 
         });
